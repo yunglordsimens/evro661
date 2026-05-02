86265a1c608c9cdbe7d261ef6c1f46af8f441fad
Masha Foreign <79109617+yunglordsimens@users.noreply.github.com>
2026-05-02 19:55:00 +0200

    scaffold: add src/data/event.js

---
+++ src/data/event.js
@@ -0,0 +1,15 @@
+export const eventConfig = {
+  name:         'євро661',
+  date:         'May 8–9, 2026',
+  venue:        'TBD',
+  address:      'TBD',
+  ticketsUrl:   '#',
+  donateUrl:    '#',
+  defaultVideo: '/footbolchik.mp4',
+  socials: {
+    ig1:  'https://www.instagram.com/sest_tisicu_ovecek',
+    ig2:  'https://www.instagram.com/evro_661',
+    ig3:  'https://www.instagram.com/resistancesupportclub',
+    sc:   '#',
+  },
+}
